73aa5194a11ce3e95eff71232c1308c4dab5febe
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2018-10-17 10:51:00 -0700

    Update 00-reduce-stringent.sh

--- 00-reduce-stringent.sh
+++ 00-reduce-stringent.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
-#
+#Run in folder containing your bins, as follows:
+# 00-reduce-stringent.sh <file extension of bins, e.g. fa, fasta> <dataset_ID> <binner>
 
-for fasta in `ls $1*$2`
-	do 
-	docker run -v `pwd`:/runbox:rw -it lpryszcz/redundans bash -c \
-	"cd runbox && /root/src/redundans/redundans.py -v --identity 0.95 --overlap 1.0 \
-	-t 30 --noscaffolding --nogapclosing --minLength 100 \
-	-f $fasta -o $(basename ${fasta} .$2)_reduced_stringent \
-	&& chown -R 1024:1024 /runbox"
+#for fasta in `ls *$1`
+#
+#	do 
+#	
+#	docker run -v `pwd`:/runbox:rw -it lpryszcz/redundans bash -c \
+#	"cd runbox && /root/src/redundans/redundans.py -v --identity 0.95 --overlap 1.0 \
+#	-t 30 --noscaffolding --nogapclosing --minLength 100 \
+#	-f $fasta -o $(basename ${fasta} .$1)_reduced_stringent \
+#	&& chown -R 1024:1024 /runbox"
+#	
+#done
+
+mkdir intermediate
+mkdir original_bins
+mkdir reduced_bins
+
+for item in `find -mindepth 1 -maxdepth 1 -type d | grep reduced_stringent`
+	
+	do
+	
+	echo cp $item/contigs.reduced.fa $2`basename $item`.fa
+	echo mv $item intermediate
+	
 done
 
-
+echo mv *fai intermediate
